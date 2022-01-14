ab50f7fc35780b27da33c6a9201934bde068a782
ronentgreat <ronenh53@gmail.com>
2022-01-14 20:55:39 +0000

    passed data from webpage to server

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+var items = ["Buy Food", "Cook Food", "Eat Food"];
+
 app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
   var today = new Date();
@@ -17,7 +21,15 @@ app.get("/", (req, res) => {
 
   var day = today.toLocaleDateString("en-US", options)
 
-  res.render("list", { kindOfDay: day });
+  res.render("list", { kindOfDay: day, newListItems: items});
+});
+
+app.post("/", (req, res) => {
+  var item = req.body.newItem;
+
+  items.push(item);
+
+  res.redirect("/");
 });
 
 app.listen(3000, () => {
